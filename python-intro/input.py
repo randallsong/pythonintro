8359c5a25d8ec8a1705c6d84885cbabf6ad44345
randallsong <randallsong149@gmail.com>
2018-09-15 14:31:12 -0400

    commit

---
+++ python-intro/input.py
@@ -0,0 +1,10 @@
+Python 3.5.2 (default, Nov 23 2017, 16:37:01) 
+[GCC 5.4.0 20160609] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> first_name = input('Please enter your first name')
+Please enter your first nameRandall
+>>> last_name = input('please enter your last name: ')
+please enter your last name: Song
+>>> print('Hello ',first_name,' ' ,last_name)
+Hello  Randall   Song
+>>> 
